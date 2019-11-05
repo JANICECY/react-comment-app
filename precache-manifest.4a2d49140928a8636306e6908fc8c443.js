cdff6c712e239d27d32d3d1d80382b783376e40d
janicecy <janicecy1217@gmail.com>
2019-11-05 13:28:25 -0500

    Updates

---
+++ precache-manifest.4a2d49140928a8636306e6908fc8c443.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b204a43e4fe67448824ee2403737e7",
+    "url": "/react-comment-app/index.html"
+  },
+  {
+    "revision": "ce3eb6632666ded472a8",
+    "url": "/react-comment-app/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "1d600450916ad3db01f9",
+    "url": "/react-comment-app/static/css/main.816aa62e.chunk.css"
+  },
+  {
+    "revision": "ce3eb6632666ded472a8",
+    "url": "/react-comment-app/static/js/2.b5d57ee4.chunk.js"
+  },
+  {
+    "revision": "1d600450916ad3db01f9",
+    "url": "/react-comment-app/static/js/main.3895f2ba.chunk.js"
+  },
+  {
+    "revision": "2986726cc8291dadce8d",
+    "url": "/react-comment-app/static/js/runtime-main.69a7650b.js"
+  }
+]);
