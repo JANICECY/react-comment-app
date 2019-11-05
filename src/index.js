7b9ff3f82cfffde7972dca2b739407d70ca9cfc0
janicecy <janicecy1217@gmail.com>
2019-11-05 15:08:25 -0500

    init

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React, { Component }from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux'
+import {createStore} from "redux"
+import commentReducer from './reducer/comments'
+import CommentApp from './container/CommentApp'
+
+
+const store = createStore(commentReducer)
+
+    class Index extends Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <CommentApp />
+            </Provider>
+
+        )
+    }
+}
+ReactDOM.render( <Provider store={store}>
+    <CommentApp />
+</Provider>, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
